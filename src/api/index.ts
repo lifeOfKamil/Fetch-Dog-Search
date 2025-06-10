70a373aa7fd5e2f329dc8247c314acaba737bae7
lifeOfKamil <lepkowskikamil@yahoo.com>
2025-06-10 13:57:52 -0500

    Upload and track remaining default vite files

---
+++ src/api/index.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { Dog, Location } from "../interfaces";
+
+const api = axios.create({ baseURL: "https://frontend-take-home-service.fetch.com", withCredentials: true });
+
+export function login(name: string, email: string) {
+	return api.post("/auth/login", { name, email });
+}
+
+export async function fetchDogs(): Promise<Dog[]> {
+	const response = await api.get<Dog[]>("/dogs/breeds");
+	console.log("Fetched dogs:", response.data);
+	return response.data;
+}
+
+export function fetchLocations(): Promise<Location[]> {
+	return api.get("/locations").then((response) => response.data);
+}
+
+export function matchDogs(ids: string[]): Promise<Dog[]> {
+	return api.post("/dogs/match", { ids }).then((response) => response.data);
+}
