20a96a759114fc44c5a68b62d6fa9e7dd1618233
lifeOfKamil <lepkowskikamil@yahoo.com>
2025-06-11 18:45:16 -0500

    feat: display all data from dogs and heart button

--- src/components/DogCard.tsx
+++ src/components/DogCard.tsx
@@ -1,29 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Dog } from "../interfaces";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { useFavorites } from "../contexts/FavoritesContext";
 import "../styles/DogCard.css";
 
 interface DogCardProps {
 	dog: Dog;
 	selected: boolean;
-	onSelect: (id: string) => void;
+	onSelect: () => void;
 }
 
-const DogCard: React.FC<DogCardProps> = ({ dog, selected, onSelect }) => {
-	const handleSeelect = () => {
-		onSelect(dog.id);
-	};
-
+const DogCard: React.FC<DogCardProps> = ({ dog, onSelect, selected }) => {
 	return (
-		<div
-			className={`dog-card ${selected ? "selected" : ""}`}
-			onClick={handleSeelect}
-			aria-pressed={selected}
-			role="button"
-			tabIndex={0}
-			onKeyPress={(e) => e.key === "Enter" && handleSelect()}
-		>
+		<div className="dog-card" onClick={onSelect}>
+			<div className="dog-card__img-wrapper">
+				<img src={dog.img} alt={`Photo of ${dog.name}`} className="dog-card__img" />
+			</div>
+
 			<div className="dog-card__info">
-				<h3>Breed: {dog}</h3>
+				<h3 className="dog-card__name">
+					{dog.name}{" "}
+					<span
+						onClick={(e) => {
+							e.stopPropagation();
+							onSelect();
+						}}
+						className={`dog-card__heart ${selected ? "dog-card__heart--active" : ""}`}
+					>
+						{selected ? <FaHeart /> : <FaRegHeart />}
+					</span>
+				</h3>
+				<p className="dog-card__breed">{dog.breed}</p>
+				<p className="dog-card__age">{dog.age} years</p>
+				<p className="dog-card__zip">
+					<FaLocationDot />
+					{dog.zip_code}
+				</p>
 			</div>
 		</div>
 	);
